2169d973ee21221a5ad6ad5c69ec756f29ae2acb
siddharth-ravi <siddharth.ravi@yahoo.com>
2018-12-06 16:54:09 +0530

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,6 +12,7 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd, double throttle) {
+	
 	this -> Kp = Kp;
 	this -> Ki = Ki;
 	this -> Kd = Kd;
@@ -22,20 +23,17 @@ void PID::Init(double Kp, double Ki, double Kd, double throttle) {
 }
 
 void PID::UpdateError(double cte) {
-	//cout << "Started Update Error" << endl;
+	
 	double cte_old = p_error;
 	p_error = cte;
 	d_error = cte - cte_old;
 	i_error += cte;
-	//cout << "Completed Update Error" << endl;
+	
 }
 
 double PID::TotalError() {
-	//cout << "Started Total Error" << endl;
-
+	
 	double totalerror;
 	totalerror = Kp * p_error + Kd * d_error + Ki * i_error;
-	//cout << "Total error" << totalerror;
-	
 	return totalerror;
 }
